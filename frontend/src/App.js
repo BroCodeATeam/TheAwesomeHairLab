32281e45424ee90b001bf5b53121d6ee83ecf32a
Philip R McDavid <31264591+FftyShadesofCode@users.noreply.github.com>
2022-05-11 21:36:34 -0400

    Can't get the navbar to style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,14 @@
+import React from 'react'
+
 import Navbar from "./components/Navbar/Navbar";
 
 import './App.css';
 
 function App() {
+
     return (
         <div className='App'>
-            <div id='preloader'>
-                <div className='loader'>
-                    <img src='images/loading.gif' width='64' alt=''/>
-                </div>
-            </div>
-            <header className='App-header'>
-                <Navbar/>
-            </header>
+            <Navbar />
         </div>
   );
 }
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,9 +1,63 @@
 import React from 'react'
 
+import Logo from '../../images/logo.jpeg'
+
+import './Navbar.css'
+
 export default function Navbar() {
     return (
-        <div>
-
-        </div>
+        <header id="header" className="header-section">
+            <div className="navbar">
+                <nav className="navbar-header">
+                    <a href="#" className="brand"><img src={Logo} alt="The Awesome Hair Lab Logo" width='75' /></a>
+                    <div className="d-flex menu-wrap align-items-center">
+                        <div id="mainmenu" className="mainmenu">
+                            <ul className="nav">
+                                <li><a data-scroll="" className="nav-link active" href="index.html">Home<span
+                                    className="sr-only">(current)</span></a>
+                                    <ul>
+                                        <li><a href="index.html">Home Default</a></li>
+                                        <li><a href="index-2.html">Home Modern</a></li>
+                                        <li><a href="index-3.html">Home Classic</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="about-us.html">About</a>
+                                    <ul>
+                                        <li><a href="about-us.html">About Us</a></li>
+                                        <li><a href="about-company.html">About Company</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="services.html">Services</a>
+                                    <ul>
+                                        <li><a href="services.html">Services 01</a></li>
+                                        <li><a href="services-2.html">Services 02</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Pages</a>
+                                    <ul>
+                                        <li><a href="appointment.html">Appointment</a></li>
+                                        <li><a href="gallery.html">Gallery</a></li>
+                                        <li><a href="team.html">Our Team</a></li>
+                                        <li><a href="pricing.html">Our Pricing</a></li>
+                                        <li><a href="404.html">404 Error</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Blog</a>
+                                    <ul>
+                                        <li><a href="blog-grid.html">Blog Grid</a></li>
+                                        <li><a href="blog-classic.html">Blog Classic</a></li>
+                                        <li><a href="blog-single.html">Blog Single</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                        <div className="header-btn">
+                            <a href="#" className="menu-btn">Make Appointment</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
     );
 };
